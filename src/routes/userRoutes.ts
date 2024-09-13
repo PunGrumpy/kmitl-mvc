bbf87e74e14e8f29619fade5a6f950720939ad90
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-09-14 02:01:54 +0700

    refactor(routes): update userRoutes to use new userService
    Simplify route definitions to use new UserService methods

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -3,21 +3,20 @@ import { Elysia, t } from 'elysia'
 import { UserService } from '@/src/services/userService'
 import { CreateUserSchema, UpdateUserSchema } from '@/src/types/user'
 
-const userService = new UserService()
-
 export const userRoutes = new Elysia({ prefix: '/users' })
-  .get('/', () => userService.getAllUsers())
-  .post('/', ({ body }) => userService.createUser(body), {
+  .use(UserService)
+  .get('/', ({ User }) => User.findMany())
+  .post('/', ({ User, body }) => User.create(body), {
     body: CreateUserSchema
   })
-  .get('/:id', ({ params: { id } }) => userService.getUserById(Number(id)), {
+  .get('/:id', ({ User, params: { id } }) => User.findUnique(Number(id)), {
     params: t.Object({
       id: t.Numeric()
     })
   })
   .put(
     '/:id',
-    ({ params: { id }, body }) => userService.updateUser(Number(id), body),
+    ({ User, params: { id }, body }) => User.update(Number(id), body),
     {
       params: t.Object({
         id: t.Numeric()
@@ -25,7 +24,7 @@ export const userRoutes = new Elysia({ prefix: '/users' })
       body: UpdateUserSchema
     }
   )
-  .delete('/:id', ({ params: { id } }) => userService.deleteUser(Number(id)), {
+  .delete('/:id', ({ User, params: { id } }) => User.delete(Number(id)), {
     params: t.Object({
       id: t.Numeric()
     })
