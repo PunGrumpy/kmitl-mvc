92df1702830cf632c088a135df8de5b68d238467
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-09-14 16:18:09 +0700

    fix(view): accident to delete renderMessage

--- src/views/CowView.ts
+++ src/views/CowView.ts
@@ -114,5 +114,12 @@ export const CowView = {
           : ''
       }
     </div>
+  `,
+
+  renderMessage: (message: string) => `
+    <div class="bg-green-100 border-l-4 border-green-500 text-green-700 p-4 mb-4" role="alert">
+      <p class="font-bold">Success</p>
+      <p>${message}</p>
+    </div>
   `
 }
